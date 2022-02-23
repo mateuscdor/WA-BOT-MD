4916d6fec23fc0acd4dabad115c3dcd3b0c608f3
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 06:29:59 +0700

    Create IMAGE-neko.js

---
+++ plugins/IMAGE-neko.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/neko')
+  if (!res.ok) throw 'Error Website sedang down'
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['neko']
+handler.tags = ['image']
+handler.command = /^(neko)$/i
+
+handler.limit = true
+
+module.exports = handler
