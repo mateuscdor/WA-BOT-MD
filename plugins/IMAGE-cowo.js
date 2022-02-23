8bc557588fbd035baa95608364d1cf94c7ba9f1a
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 00:41:52 +0700

    Update IMAGE-cowo.js

--- plugins/IMAGE-cowo.js
+++ plugins/IMAGE-cowo.js
@@ -2,17 +2,17 @@ const fetch = require('node-fetch')
 
 let handler = async (m, { conn }) => {
     try {
-        let res = await fetch(global.API('xteam', '/randomimage/cewe', {}, 'APIKEY'))
+        let res = await fetch(global.API('xteam', '/randomimage/cowo', {}, 'APIKEY'))
         if (res.status != 200) throw await res.text()
         let img = await res.buffer()
-        conn.sendFile(m.chat, img, '', '*Nee...*', m, false, { thumbnail: Buffer.alloc(0) })
+        conn.sendFile(m.chat, img, '', '*Nee*', m, false, { thumbnail: Buffer.alloc(0) })
     } catch (e) {
         throw `Limit apikey habis atau error!`
     }
 }
-handler.help = ['cewe']
+handler.help = ['cowo']
 handler.tags = ['internet']
-handler.command = /^cewe$/i
+handler.command = /^(cowo)$/i
 handler.limit = true
 
 module.exports = handler
