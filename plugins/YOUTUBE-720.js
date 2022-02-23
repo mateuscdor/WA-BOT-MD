f9c8e67f10d918b12491cad3e27e765fc70f68e7
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 10:24:29 +0700

    Update YOUTUBE-720.js

--- plugins/YOUTUBE-720.js
+++ plugins/YOUTUBE-720.js
@@ -4,10 +4,9 @@ let handler = async (m, { conn, command, text }) => {
     if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
   let res = await xfar.Youtube(text)
 m.reply('*Tunggu Sebentar...*')
-conn.sendFile(m.chat,res.medias[2].url, '', `Youtube Downloader
-720p
-jika masih blur, berarti videonya emang ngeblur dari youtubenya
-Atau bisa jadi faktor *MUKA*`, m)
+conn.sendFile(m.chat,res.medias[2].url, '', `YOUTUBE DOWNLOADER
+720p Quality
+jika masih blur, berarti videonya.`, m)
 
 }
 handler.help = ['ytv720 <url>']
