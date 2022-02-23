38072554ae4bbacfea643934b5eec301d719f877
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 08:51:08 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,43 +1,42 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys-md')
-let fs = require('fs')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let handler = async (m) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender
-    let user = global.db.data.users[who]
-let anu = `*─────� DONATE 」 ───*
+let duit = `*────── 「 DONATE 」 ──────*
 
 Hai 👋
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├📌Indoosat: 085643354073
+├🏧 0895426157070 (OVO/Dana/GoPay)
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 
 Arigatou!
 
 Contact person Owner:
-wa.me/6285643354073 (Owner)
-
-*donasi via follow ig juga boleh*`
+wa.me/62895426157070 (Owner)`
+let message = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/ca517a39ce85c8a4c0479.jpg' }}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
      templateMessage: {
          hydratedTemplate: {
-           hydratedContentText: anu,
-           locationMessage: { 
-           jpegThumbnail: fs.readFileSync('./media/donasi.jpg') }, 
+           imageMessage: message.imageMessage,
+           hydratedContentText: duit,
            hydratedFooterText: wm,
            hydratedButtons: [{
              urlButton: {
-               displayText: '📍instagram',
-               url: instagram
+               displayText: '🏧 Saweria',
+               url: 'https://saweria.co/MxShimmer01'
              }
 
            },
                {
+             callButton: {
+               displayText: 'Tri',
+               phoneNumber: '+62 895-4261-57070'
+             }
+           },           
+               {
              quickReplyButton: {
-               displayText: 'Back To Menu',
-               id: '.menu',
+               displayText: '🧒 Owner',
+               id: '.owner',
              }
 
            }]
@@ -51,8 +50,9 @@ wa.me/6285643354073 (Owner)
          { messageId: template.key.id }
      )
 }
-handler.help = ['donasi', 'donate']
-handler.tags = ['xp']
-handler.command = /^(donasi|donate)$/i
+
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)|bagiduit$/i
 
 module.exports = handler
