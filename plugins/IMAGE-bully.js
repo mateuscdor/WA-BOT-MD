aa287d8ef62cad8bc23cd77d918d6d9d34aea883
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 06:33:50 +0700

    Create IMAGE-bully.js

---
+++ plugins/IMAGE-bully.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/sfw/bully')
+  if (!res.ok) throw 'Error Website sedang down'
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['bully']
+handler.tags = ['image']
+handler.command = /^(bully)$/i
+
+handler.limit = true
+
+module.exports = handler
