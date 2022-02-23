bc69d2fd3d7aa4a24ebc2dda05f3e1f66a2200bc
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 00:11:57 +0700

    Update IMAGE-wpmobile.js

--- plugins/IMAGE-wpmobile.js
+++ plugins/IMAGE-wpmobile.js
@@ -1,26 +1,18 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, args }) => {
-   response = args.join(' ')
-  if (!args) throw 'Masukkan Parameter'
-  m.reply('Sedang Diproses...')
-  let res = `https://api.xteam.xyz/randomimage/wpmobile?apikey=RootSmile7`
-  conn.sendFile(m.chat, res, 'wpmobile.jpg', `wangy wangy wangy`, m, false)
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn }) => {
+    try {
+        let res = await fetch(global.API('xteam', '/randomimage/wpmobile', {}, 'APIKEY'))
+        if (res.status != 200) throw await res.text()
+        let img = await res.buffer()
+        conn.sendFile(m.chat, img, '', '*Nee*', m, false, { thumbnail: Buffer.alloc(0) })
+    } catch (e) {
+        throw `Limit apikey habis atau error!`
+    }
 }
-handler.help = ['wpmobile'].map(v => v + ' ')
-handler.tags = ['image']
-
-handler.command = /^(wpmobile)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = true
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+handler.help = ['wallpaperanime']
+handler.tags = ['internet']
+handler.command = /^(wallpaper|wp)anime$/i
 handler.limit = true
 
 module.exports = handler
-
