c3f3327828dcc36b23fee49ff79e353b1822b5eb
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 06:25:01 +0700

    Update and rename waifu.js to IMAGE-waifu.js

--- plugins/IMAGE-waifu.js
+++ plugins/IMAGE-waifu.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
 }
 handler.help = ['waifu']
-handler.tags = ['anime']
+handler.tags = ['image']
 handler.command = /^(waifu)$/i
 
 handler.limit = true
