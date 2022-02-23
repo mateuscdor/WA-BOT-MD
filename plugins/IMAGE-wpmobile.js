d7cfad549429d16caa46ca33d1622389b22af104
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 09:15:08 +0700

    Update and rename wpmobile.js to IMAGE-wpmobile.js

--- plugins/IMAGE-wpmobile.js
+++ plugins/IMAGE-wpmobile.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ')
   if (!args) throw 'Masukkan Parameter'
   m.reply('Sedang Diproses...')
-  let res = `https://api.xteam.xyz/randomimage/wpmobile?apikey=beliapikey`
+  let res = `https://api.xteam.xyz/randomimage/wpmobile?apikey=RootSmile7`
   conn.sendFile(m.chat, res, 'wpmobile.jpg', `wangy wangy wangy`, m, false)
 }
 handler.help = ['wpmobile'].map(v => v + ' ')
