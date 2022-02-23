f6eb22db43a333e167a5bc18144df9be69395855
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 14:50:45 +0700

    Update SPAM-call.js

--- plugins/SPAM-call.js
+++ plugins/SPAM-call.js
@@ -1,8 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix }) => {
-  if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx limit 3kali tiap nomer`
+  if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
   let nomor = text.replace(/[^0-9]/gi, '').slice(2)
-  if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx limit 3kali tiap nomer`
+  if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
   m.reply('✨Tunggu permintaan anda sedang diproses...')
   let anu = await fetch(`https://id.jagreward.com/member/verify-mobile/${nomor}`).then(a => a.json())
   let spcall = `*Nomor bot* : _${anu.phone_prefix}_\n\n_📞Bot berhasil menelpon anda!_`
