348dcc33e5c4b845a5451779a39aefadc7cb0fae
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 11:41:32 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,10 +1,10 @@
 let handler = async(m, { conn, text, participants }) => {
   let teks = `⋙ *Pesan dari Admin Group* ⋘
-\n *${text ? text : 'Nothing'}*\n\n`
+\n *${text ? text : 'Cuma Gabut'}*\n\n`
 		      	for (let mem of participants) {
 		            teks += ` @${mem.id.split('@')[0]}\n`
 				}
-                teks += `\nbukan elyas`
+                teks += `\njangan salahin bot salahin adminnya`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
