e24115df75572bfdacc1c2dbea1b60407a86e072
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 09:12:32 +0700

    Update Ok.js

--- plugins/Ok.js
+++ plugins/Ok.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/f61a7c96f3f86b6c5214f.png', m, { packname: "sticker by", author: "Elyas" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/c4e14d96a28213f8f785a.jpg', m, { packname: "", author: "" })
 }
 
-handler.customPrefix = /^(ok)$/i
+handler.customPrefix = /^(ok|nice)$/i
 handler.command = new RegExp
 
 module.exports = handler
