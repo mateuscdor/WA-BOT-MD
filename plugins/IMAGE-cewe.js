e86b77f81c84f2f89a884199e1bb561cc3f0707e
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 06:23:23 +0700

    Update IMAGE-cewe.js

--- plugins/IMAGE-cewe.js
+++ plugins/IMAGE-cewe.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
     }
 }
 handler.help = ['cewe']
-handler.tags = ['internet']
+handler.tags = ['image']
 handler.command = /^(cewe)$/i
 handler.limit = true
 
