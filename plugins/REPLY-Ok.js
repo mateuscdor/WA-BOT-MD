0f4c5be38c68dfbbf809416d44068b9e23897468
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-23 10:42:58 +0700

    Update REPLY-Ok.js

--- plugins/REPLY-Ok.js
+++ plugins/REPLY-Ok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/c4e14d96a28213f8f785a.jpg', m, { packname: "", author: "" })
 }
 
-handler.customPrefix = /^(ok|nice)$/i
+handler.customPrefix = /^(ok)$/i
 handler.command = new RegExp
 
 module.exports = handler
