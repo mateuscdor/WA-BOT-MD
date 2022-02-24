e3d76adb67387c372995e50607b6ccb68928b3a7
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 09:18:08 +0700

    Update and rename REPLY-Welcome.js to REPLY-Intro.js

--- plugins/REPLY-Intro.js
+++ plugins/REPLY-Intro.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/e545d83b4dcc4122199d0.png', m, { packname: "sticker by", author: "Yuu Otosaka" })
 }
 
-handler.customPrefix = /^(welcome)$/i
+handler.customPrefix = /^(intro)$/i
 handler.command = new RegExp
 
 module.exports = handler
