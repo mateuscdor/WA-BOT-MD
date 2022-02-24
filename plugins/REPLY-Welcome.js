b31ddaa829925f8e93dd2d5a31631d485a8cb255
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 08:59:25 +0700

    Update REPLY-Welcome.js

--- plugins/REPLY-Welcome.js
+++ plugins/REPLY-Welcome.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/e545d83b4dcc4122199d0.jpg', m, { packname: "sticker by", author: "Yuu Otosaka" })
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/e545d83b4dcc4122199d0.png', m, { packname: "sticker by", author: "Yuu Otosaka" })
 }
 
 handler.customPrefix = /^(welcome)$/i
