a7e65669718723680e72b466237a96475703e3a9
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 08:53:43 +0700

    Create REPLY-Welcome.js

---
+++ plugins/REPLY-Welcome.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+
+conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/e545d83b4dcc4122199d0.jpg', m, { packname: "sticker by", author: "Yuu Otosaka" })
+}
+
+handler.customPrefix = /^(welcome)$/i
+handler.command = new RegExp
+
+module.exports = handler
