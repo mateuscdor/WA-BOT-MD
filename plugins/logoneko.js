b062221f080f64c166496a7e553666ac6da4c16c
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 08:09:33 +0700

    Update logoneko.js

--- plugins/logoneko.js
+++ plugins/logoneko.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko elyas|ganzz'
+  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko shimmer|cans'
   m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=ELYASXD`
+  let res = `https://caliphapi.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=fwA4UVbh`
   conn.sendFile(m.chat, res, 'nekologo.jpg', `Sudah jadi`, m, false)
 }
 handler.help = ['logoneko'].map(v => v + ' <text|text>')
