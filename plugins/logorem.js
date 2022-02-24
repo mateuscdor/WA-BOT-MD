31c03e8d8f402d3fdcabaed89424796064de0bee
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 08:10:35 +0700

    Update logorem.js

--- plugins/logorem.js
+++ plugins/logorem.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ')
   if (!args[0]) throw 'Masukkan Text'
   m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/rem?text=${response}&text2=Elyas&apikey=ELYASXD`
+  let res = `https://caliphapi.com/api/rem?text=${response}&text2=Shimmer&apikey=fwA4UVbh`
   conn.sendFile(m.chat, res, 'kaneki.jpg', `Sudah jadi`, m, false)
 }
 handler.help = ['logorem'].map(v => v + ' <teks>')
