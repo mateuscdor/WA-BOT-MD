34ef252e91766fc9c5ec8c6aba1089f9b2d14472
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-24 08:04:39 +0700

    Update logololi.js

--- plugins/logololi.js
+++ plugins/logololi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
   if (!args[0]) throw 'Masukkan Parameter'
   m.reply('*Wait Ngab*\nProses...')
-  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=wVLiUzSa`
+  let res = `https://caliphapi.com/api/lolimaker?text=${response[0]}&text2=elyas&apikey=fwA4UVbh`
   conn.sendFile(m.chat, res, 'lolilogo.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['logololi'].map(v => v + ' <text>')
