3adbcd0a7926558c562178a7e4f1acb59f795f85
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-25 10:54:44 +0700

    Update REPLY-Ok.js

--- plugins/REPLY-Ok.js
+++ plugins/REPLY-Ok.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
-conn.sendImageAsSticker(m.chat, 'https://telegra.ph/file/d7ef7ab69cd186b4abe3a.png', m, { packname: "sticker by", author: "Yuu Otosaka" })
+conn.sendGifAsSticker(m.chat, 'https://telegra.ph/file/7627909d55b01b67e2b32.gif', m, { packname: "sticker by", author: "Yuu Otosaka" })
 }
 
 handler.customPrefix = /^(ok)$/i
