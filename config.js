d742df81b075d9eeeb78d9ab58ba5265bd35820a
Mx-Shimmer-01 <89338571+Mx-Shimmer-01@users.noreply.github.com>
2022-02-25 08:34:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-global.owner = ['62895426157070',''] // Put your number here
+global.owner = ['62895426157070','79588852186'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
